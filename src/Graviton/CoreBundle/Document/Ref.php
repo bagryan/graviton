0c064c1b8ce3541113dd636bbea25b39801d50fb
narcoticfresh <wee@xbe.ch>
2015-03-11 16:48:16 +0100

    new ref document..

---
+++ src/Graviton/CoreBundle/Document/Ref.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Graviton\CoreBundle\Document;
+
+class Ref
+{
+
+    protected $ref;
+
+    /**
+     * @return mixed
+     */
+    public function getRef()
+    {
+        return $this->ref;
+    }
+
+    /**
+     * @param mixed $ref
+     */
+    public function setRef($ref)
+    {
+        $this->ref = $ref;
+    }
+
+
+
+}
